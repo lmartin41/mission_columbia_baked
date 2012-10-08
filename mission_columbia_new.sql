090475cef8a0bcf507998d0211f2d7253190a9c7
Lee Benson Martin <lmartin41@gatech.edu>
2012-10-08 14:15:08 -0400

    pushing sql file

---
+++ mission_columbia_new.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 08, 2012 at 04:38 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `mission_columbia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acos`
+--
+
+CREATE TABLE IF NOT EXISTS `acos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `model` varchar(255) DEFAULT '',
+  `foreign_key` int(10) unsigned DEFAULT NULL,
+  `alias` varchar(255) DEFAULT '',
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aros`
+--
+
+CREATE TABLE IF NOT EXISTS `aros` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `model` varchar(255) DEFAULT '',
+  `foreign_key` int(10) unsigned DEFAULT NULL,
+  `alias` varchar(255) DEFAULT '',
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aros_acos`
+--
+
+CREATE TABLE IF NOT EXISTS `aros_acos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `aro_id` int(10) unsigned NOT NULL,
+  `aco_id` int(10) unsigned NOT NULL,
+  `_create` char(2) NOT NULL DEFAULT '0',
+  `_read` char(2) NOT NULL DEFAULT '0',
+  `_update` char(2) NOT NULL DEFAULT '0',
+  `_delete` char(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `DOB` datetime NOT NULL,
+  `sex` varchar(6) NOT NULL,
+  `SSN` int(11) DEFAULT NULL COMMENT 'not required',
+  `address_one` varchar(30) DEFAULT NULL,
+  `address_two` varchar(30) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `state` varchar(2) DEFAULT NULL,
+  `zip` int(11) DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `apartment_number` int(6) DEFAULT NULL,
+  `how_did_you_hear` text,
+  `how_long_do_you_need` text,
+  `isDeleted` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_incomes`
+--
+
+CREATE TABLE IF NOT EXISTS `client_incomes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `regular_job` decimal(11,0) DEFAULT NULL,
+  `food_stamps` decimal(11,0) DEFAULT NULL,
+  `veterans_pension` decimal(11,0) DEFAULT NULL,
+  `part_time_job` decimal(11,0) DEFAULT NULL,
+  `social_security` decimal(11,0) DEFAULT NULL,
+  `annuity_check` decimal(11,0) DEFAULT NULL,
+  `child_support` decimal(11,0) DEFAULT NULL,
+  `ssi_or_disability` decimal(11,0) DEFAULT NULL,
+  `unemployment` decimal(11,0) DEFAULT NULL,
+  `when_next_check` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_relations`
+--
+
+CREATE TABLE IF NOT EXISTS `client_relations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `relationship` varchar(20) NOT NULL COMMENT '(as in daughter, mother, son, sister, etc.)',
+  `DOB` datetime NOT NULL,
+  `sex` varchar(6) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_specifics`
+--
+
+CREATE TABLE IF NOT EXISTS `client_specifics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `pregnant` tinyint(1) DEFAULT NULL,
+  `disabled` tinyint(1) DEFAULT NULL,
+  `handicapped` tinyint(1) DEFAULT NULL,
+  `stove` tinyint(1) DEFAULT NULL,
+  `refrigerator` tinyint(1) DEFAULT NULL,
+  `cell` int(20) DEFAULT NULL,
+  `cable` tinyint(1) DEFAULT NULL,
+  `internet` tinyint(1) DEFAULT NULL,
+  `car_truck_model` varchar(20) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks`
+--
+
+CREATE TABLE IF NOT EXISTS `feedbacks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `feedback` text NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organizations`
+--
+
+CREATE TABLE IF NOT EXISTS `organizations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_name` varchar(30) NOT NULL,
+  `org_type` varchar(20) NOT NULL COMMENT 'as in legal, health, other, etc.',
+  `address_one` varchar(30) NOT NULL,
+  `address_two` varchar(30) DEFAULT NULL,
+  `city` varchar(30) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `zip` int(11) NOT NULL,
+  `contact` varchar(30) NOT NULL,
+  `website` varchar(30) DEFAULT NULL,
+  `phone` int(20) NOT NULL,
+  `phone_cell` int(20) DEFAULT NULL,
+  `phone_office` int(20) DEFAULT NULL,
+  `isDeleted` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resources`
+--
+
+CREATE TABLE IF NOT EXISTS `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resource_name` varchar(30) NOT NULL,
+  `organization_id` int(11) NOT NULL,
+  `resource_status` text NOT NULL,
+  `isDeleted` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `agency_id` (`organization_id`),
+  KEY `agency_id_2` (`organization_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resource_uses`
+--
+
+CREATE TABLE IF NOT EXISTS `resource_uses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `comments` text,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `client_id` (`client_id`),
+  KEY `resource_id` (`resource_id`),
+  KEY `date` (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique Identifier',
+  `username` varchar(50) NOT NULL,
+  `password` char(40) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL,
+  `isSuperAdmin` tinyint(1) NOT NULL,
+  `organization_id` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `isDeleted` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `agency_id` (`organization_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1237 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `isAdmin`, `isSuperAdmin`, `organization_id`, `email`, `isDeleted`, `created`, `modified`) VALUES
+(1234, 'samwise', '31c47d09c29bec8ae9c960b797c44cae294a8b4e', 0, 0, 0, '', 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `client_incomes`
+--
+ALTER TABLE `client_incomes`
+  ADD CONSTRAINT `client_incomes_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+--
+-- Constraints for table `client_relations`
+--
+ALTER TABLE `client_relations`
+  ADD CONSTRAINT `client_relations_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+--
+-- Constraints for table `client_specifics`
+--
+ALTER TABLE `client_specifics`
+  ADD CONSTRAINT `client_specifics_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+--
+-- Constraints for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD CONSTRAINT `feedbacks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `resources`
+--
+ALTER TABLE `resources`
+  ADD CONSTRAINT `resources_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`);
+
+--
+-- Constraints for table `resource_uses`
+--
+ALTER TABLE `resource_uses`
+  ADD CONSTRAINT `resource_uses_ibfk_2` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`),
+  ADD CONSTRAINT `resource_uses_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
