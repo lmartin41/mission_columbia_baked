8dae458f87d42035fdae8baa0e7d74625c4189b2
Lee Benson Martin <lmartin41@gatech.edu>
2012-10-25 12:23:20 -0400

    welcomecontroller

---
+++ Controller/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class WelcomeController extends AppController {
+    
+    public function index() {
+        
+    }
+}
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
