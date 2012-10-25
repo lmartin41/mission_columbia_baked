a8e48cc0558db604f58ca990155eb70727ee5b34
Lee Benson Martin <lmartin41@gatech.edu>
2012-10-24 22:05:16 -0400

    woops

--- Config/database.php
+++ Config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => '',
+		'password' => 'Samwyse6553426',
 		'database' => 'mission_columbia',
 	);
 }
